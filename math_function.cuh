390f0e0b59df4a2d22b3c313e08a287768d7d000
greydoubt <43443470+greydoubt@users.noreply.github.com>
2024-09-13 21:27:02 -0700

    Create math_function.cuh
    atomic, basic, categorical, and templated math function object

---
+++ math_function.cuh
@@ -0,0 +1,11 @@
+// math_function.cuh
+#ifndef MATHFUNCTION_H_
+#define MATHFUNCTION_H_
+template <class Res>
+class MathFunction {
+public:
+        MathFunction();
+        virtual ~MathFunction();
+        virtual Res operator()(Res value) = 0;
+};
+#endif 
